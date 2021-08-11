612246472a6fb38870692695ce00aabf3e97c828
Victor Cardins <vcardins@provantagex.com>
2021-08-11 14:41:06 -0700

    (misc): Cleanup

--- libs/shared/types/src/models/household/HouseholdOutput.ts
+++ libs/shared/types/src/models/household/HouseholdOutput.ts
@@ -1,9 +1,5 @@
 import { HouseholdInput } from './HouseholdInput';
-import { HouseholdRoomOutput } from './HouseholdRoomOutput';
-import { HouseholdMemberOutput } from './HouseholdMemberOutput';
 
 export class HouseholdOutput extends HouseholdInput {
 	id: number;
-	rooms: HouseholdRoomOutput[];
-	members: HouseholdMemberOutput[];
 }
--- libs/shared/types/src/models/tasks/TaskTemplateOutput.ts
+++ libs/shared/types/src/models/tasks/TaskTemplateOutput.ts
@@ -3,7 +3,6 @@ import { TaskTemplateInput } from './TaskTemplateInput';
 export class TaskTemplateOutput extends TaskTemplateInput {
 	id: number;
 	roomTypeId: number;
-	frequencyId: number;
 }
 
 export type TaskTemplateOutputMapped = Record<number, TaskTemplateOutput[]>;
